395ce7d5046ed485defcd9b67726f133d31605b7
MartynGorski <blathasarbeltthesupreme@gmail.com>
2023-01-18 15:36:06 +0100

    export fix

--- sd2tool.py
+++ sd2tool.py
@@ -14,14 +14,19 @@ def importdata(filename):
         lines = [line.rstrip() for line in file]
     for j, i in enumerate(lines):
         if re.search('expor.+', i):
-            i = i.strip("export ")
-            i = i.rstrip(" is TDeckDescriptor")
+            i = i.lstrip("export ")
+            i = i[:-19]
+            #i = i.rstrip("TDeckDescriptor")
+            #i = i.rstrip("is ")
             MainDescriptor.append(i)
             MainDscCounter += 1
             Quantity.append([])
             DeckDivision.append(lines[j+1])
             DeckIdentifier.append(lines[j+2])
-            Superior.append(lines[j+3])
+            if re.search('Superior =',lines[j+3]):
+                Superior.append(lines[j+3])
+            else:
+                Superior.append("Pass")
         if re.search('Quanti.+', i):
             i = i.lstrip("Quantity = ")
             TempArray.append(i)
@@ -36,19 +41,21 @@ def importdata(filename):
 def exportdata():
     global MainDescriptor, DeckDivision, DeckIdentifier, Superior, Quantity, TempArray, MainDscCounter
     f1 = open("exporttest.ndf", "w")
+    f1.write("// Ne pas éditer, ce fichier est auto-généré !\n\n\n")
     for i, j in enumerate(MainDescriptor):
         f1.write(f"export {j} is TDeckDescriptor\n(\n")
-        f1.write(f"\tDeckDivision = {DeckDivision[i]}\n")
-        f1.write(f"\tDeckIdentifier = {DeckIdentifier[i]}\n")
-        f1.write(f"\tSuperior = {Superior[i]}\n")
-        f1.write(f"\tDeckPackList = \n\t[\n")
+        f1.write(f"    {DeckDivision[i]}\n")
+        f1.write(f"    {DeckIdentifier[i]}\n")
+        if Superior[i] != "Pass":
+            f1.write(f"    {Superior[i]}\n")
+        f1.write(f"    DeckPackList =\n    [\n")
         for k, l in enumerate(Quantity[i]):
-            f1.write(f"\t\tTDeckPackDescription\n\t\t(\n")
+            f1.write(f"        TDeckPackDescription\n        (\n")
             temp = Quantity[i][k]
-            f1.write(f"\t\t\tQuantity = {temp[0]}\n")
-            f1.write(f"\t\t\tDeckPack = {temp[1]}\n")
-            f1.write(f"\t\t\tExperienceLevel = {temp[2]}\n\t\t),\n")
-        f1.write(f"\t]\n)\n")
+            f1.write(f"            Quantity = {temp[0]}\n")
+            f1.write(f"            DeckPack = {temp[1]}\n")
+            f1.write(f"            ExperienceLevel = {temp[2]}\n        ),\n")
+        f1.write(f"    ]\n)\n")
 
-importdata("testfile3.txt")        
+importdata("sorteddecks.ndf") 
 exportdata()
